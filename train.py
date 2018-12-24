f1ac6497d0e43897341c01cccb664c3aabc73b50
kihy <kh695640204@gmail.com>
2018-12-24 15:30:22 +0800

    refactored train to use config script

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+from configparser import ConfigParser, ExtendedInterpolation
 from datetime import datetime
 from math import ceil
 
@@ -9,7 +10,6 @@ matplotlib.use("Agg")
 
 from keras_retinanet.preprocessing.csv_generator import CSVGenerator
 
-
 # import keras
 import keras
 # import keras_retinanet
@@ -18,49 +18,59 @@ from keras_retinanet import models, losses
 # set tf backend to allow memory to grow, instead of claiming everything
 import tensorflow as tf
 
+
 def get_session():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    return tf.Session(config=config)
+    return tf.Session(config = config)
+
+
+parser = ConfigParser(interpolation = ExtendedInterpolation())
+parser.read("config.ini")
+params = parser["train"]
+backbone=params["backbone"]
 
 # use this environment flag to change which GPU to use
-#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # set the modified tf session as backend in keras
 keras.backend.tensorflow_backend.set_session(get_session())
 
-
-model = models.backbone('resnet50').retinanet(num_classes=1)
+model = models.backbone(backbone).retinanet(num_classes = 1)
 model.compile(
-    loss={
-        'regression'    : losses.smooth_l1(),
+    loss = {
+        'regression': losses.smooth_l1(),
         'classification': losses.focal()
     },
-    optimizer=keras.optimizers.adam(lr=1e-5, clipnorm=0.001)
+    optimizer = keras.optimizers.adam(lr = 1e-5, clipnorm = 0.001)
 )
 
-weights="weights/resnet50_coco_best_v2.1.0.h5"
-model.load_weights(weights, by_name=True, skip_mismatch=True)
-batch_size=16
-data = CSVGenerator("train_annotation.csv","class_map.csv", batch_size = batch_size, image_min_side = 512, image_max_side = 512)
-val_data = CSVGenerator("val_annotation.csv","class_map.csv", batch_size = batch_size, image_min_side = 512, image_max_side = 512)
-val_dataset_size=val_data.size()
+weights = params["weights_path"]
+model.load_weights(weights, by_name = True, skip_mismatch = True)
+batch_size = int(params["batchsize"])
+
+data = CSVGenerator("train_annotation.csv", "class_map.csv", batch_size = batch_size, image_min_side = 512,
+                    image_max_side = 512)
+val_data = CSVGenerator("val_annotation.csv", "class_map.csv", batch_size = batch_size, image_min_side = 512,
+                        image_max_side = 512)
+
+val_dataset_size = val_data.size()
 
 # TODO: Set the file path under which you want to save the model.
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M').split(" ")
 model_checkpoint = ModelCheckpoint(
-    filepath = os.path.join("snapshots",
+    filepath = os.path.join(params["check_point_path"],
                             'retinanet_fire_{}_{}.h5'.format(current_time[0], current_time[1])),
     monitor = 'val_loss',
     verbose = 1,
     save_best_only = True,
     save_weights_only = False,
     mode = 'auto',
-    period = 1)
+    period = 10)
 # model_checkpoint.best =
 
 csv_logger = CSVLogger(
-    filename = os.path.join("csv_logs", 'retinanet_fire_{}_{}.csv'.format(current_time[0], current_time[1])),
+    filename = os.path.join(params["csv_path"], 'retinanet_fire_{}_{}.csv'.format(current_time[0], current_time[1])),
     separator = ',',
     append = True)
 
@@ -69,7 +79,7 @@ csv_logger = CSVLogger(
 terminate_on_nan = TerminateOnNaN()
 
 tensorboard = TensorBoard(
-    log_dir = os.path.join("tensorboard", 'retinanet', current_time[0], current_time[1]),
+    log_dir = os.path.join(params["tensorboard_path"], 'retinanet', current_time[0], current_time[1]),
     write_images = True, write_graph = True)
 
 callbacks = [model_checkpoint,
@@ -81,8 +91,8 @@ callbacks = [model_checkpoint,
 
 # If you're resuming a previous training, set `initial_epoch` and `final_epoch` accordingly.
 initial_epoch = 0
-final_epoch = 20
-steps_per_epoch = 86
+final_epoch = int(params["epochs"])
+steps_per_epoch = int(params["steps_per_epoch"])
 
 history = model.fit_generator(generator = data,
                               steps_per_epoch = steps_per_epoch,
@@ -91,5 +101,3 @@ history = model.fit_generator(generator = data,
                               validation_data = val_data,
                               validation_steps = ceil(val_dataset_size / batch_size),
                               initial_epoch = initial_epoch)
-
-
