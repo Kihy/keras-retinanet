7dc247fc1db01ff964925b69237f380c64a8a719
kihy <kh695640204@gmail.com>
2018-12-24 15:40:55 +0800

    refactored eval to use config script

--- eval.py
+++ eval.py
@@ -1,4 +1,6 @@
 import os
+from configparser import ConfigParser, ExtendedInterpolation
+
 import cv2
 import numpy as np
 import matplotlib
@@ -14,15 +16,20 @@ from keras_retinanet.utils.colors import label_color
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
 from keras_retinanet.utils.visualization import draw_box, draw_caption
 
-image_dir = "dataset/fire_1720/JPEGImages"
-model_path = os.path.join('snapshots', 'retinanet_fire_2018-12-24_11:25.h5')
+parser = ConfigParser(interpolation = ExtendedInterpolation())
+parser.read("config.ini")
+params = parser["eval"]
+
+image_dir = params["image_dir"]
+model_path = params["model_path"]
 
 model = models.load_model(model_path, backbone_name = 'resnet50')
 model = convert_model(model)
 labels_to_names = {0: 'fire'}
-num_file = 20
-test_file_path = "dataset/fire_1720/ImageSets/Main/test.txt"
+num_file = int(params["num_file"])
+test_file_path = params["image_set_path"]
 f = open(test_file_path, "r")
+
 for line in f.readlines()[:num_file]:
     filename = line.strip()
     image = read_image_bgr(os.path.join(image_dir, filename + ".jpg"))
